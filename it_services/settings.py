3d5a824eff6cfbec43a0e56b969e26ee98a0097e
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 12:59:59 +0530

    feat: add a base template

--- it_services/settings.py
+++ it_services/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "it_services.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
