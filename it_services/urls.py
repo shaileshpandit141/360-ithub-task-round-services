c508eb70d1c58f1c6f7c43bb6954d6a1837d2286
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 16:49:31 +0530

    core: enable serving of user-uploaded media files

--- it_services/urls.py
+++ it_services/urls.py
@@ -2,6 +2,8 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from service_app import urls as service_app_urls
 from subscription_app import urls as subscription_app_urls
 from user_app import urls as user_app_urls
@@ -12,3 +14,7 @@ urlpatterns = [
     path("", include(subscription_app_urls)),
     path("", include(user_app_urls)),
 ]
+
+# Enable serving of user-uploaded media files
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
