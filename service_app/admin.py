2dabefcf1c2399d688971d4bb2335d941c9c8794
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 12:43:55 +0530

    feat: register Service model to django admin

--- service_app/admin.py
+++ service_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Service
+
 
 # Register your models here.
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'active')
