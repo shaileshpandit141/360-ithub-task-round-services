bd07e3243b563498eba0234f0d7cdf08e1086afd
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 13:49:16 +0530

    core: export all views as module

---
+++ user_app/views/__init__.py
@@ -0,0 +1,11 @@
+from .login_view import login_view
+from .logout_view import logout_view
+from .register_view import register_view, otp_email_verification_view
+
+
+__all__ =  [
+    "login_view",
+    "logout_view",
+    "register_view",
+    "otp_email_verification_view",
+]
