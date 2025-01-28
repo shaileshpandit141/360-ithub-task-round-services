9fca6b06674702ea2da73f439238369aee35c870
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 12:38:33 +0530

    core: start the subscription app and setup the urls

--- it_services/urls.py
+++ it_services/urls.py
@@ -3,8 +3,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from service_app import urls as service_app_urls
+from subscription_app import urls as subscription_app_urls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(service_app_urls)),
+    path("", include(subscription_app_urls)),
 ]
---
+++ subscription_app/urls.py
@@ -0,0 +1,5 @@
+"""URL configuration for subscription app."""
+
+from django.urls import path
+
+urlpatterns = [ ]
