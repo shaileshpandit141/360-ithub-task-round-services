8b9ec410757dd8fa1c864e566815982c32a58f63
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-01-28 12:44:55 +0530

    feat: register Subscription model to django admin

--- subscription_app/admin.py
+++ subscription_app/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Subscription
+
 
 # Register your models here.
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'service', 'get_payment_status')
+
+    def get_payment_status(self, obj):
+        return obj.payment_status
+
+    setattr(get_payment_status, "short_description", "Payment Status")
