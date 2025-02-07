98c069942c366c0d4d716bee82392c4da00f5ebe
shaileshpandit141 <shaileshpandit141@gmail.com>
2025-02-07 09:18:06 +0530

    feat: hide the update button if service is active

--- service_app/views/service_detail_view.py
+++ service_app/views/service_detail_view.py
@@ -1,18 +1,22 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from ..models import Service
+from subscription_app.models import Subscription
 from django.contrib.auth.decorators import login_required
 
 
 @login_required(login_url="login")
 def service_detail_view(request, pk):
     """Display the detailed view of a service including its calculated net price."""
-    
+
     # Get service or return 404 if not found
     service = get_object_or_404(Service, pk=pk)
+
+    is_buy = Subscription.objects.filter(service=service.id).exists()
 
     # Calculate net price with tax included
     net_price = float(service.price) + (float(service.price) * (float(service.tax) / 100))
     setattr(service, "amount", net_price)
+    setattr(service, "is_buy", is_buy)
 
     return render(request, "service_app/service_detail.html", {"service": service})
